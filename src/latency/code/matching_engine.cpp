852577cc77a14aecda8035a0118e44a2aed79892
odenpetersen <oden.petersen@gmail.com>
2026-09-08 17:33:05 +1000

    update

--- src/latency/code/matching_engine.cpp
+++ src/latency/code/matching_engine.cpp
@@ -2,21 +2,20 @@
 #include <netinet/in.h>   // sockaddr_in, INADDR_ANY, IPPROTO_TCP
 #include <arpa/inet.h>    // htons(), inet_pton()
 #include <unistd.h>       // close()
-#include <cstring>        // strlen()
-#include <iostream>       // std::cout
+#include <thread>         // std::thread
+#include <iostream>       // std::cin, std::cout
 #include <string>         // std::string
 
 class Peer {
-    int listen_fd;
     int fd;
 
 public:
-    Peer(int local_port, const char* peer_ip, int peer_port) {
-        listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    Peer(int port, const char* peer_ip) {
+        int listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
         sockaddr_in local{};
         local.sin_family = AF_INET;
-        local.sin_port = htons(local_port);
+        local.sin_port = htons(port);
         local.sin_addr.s_addr = INADDR_ANY;
 
         bind(listen_fd, (sockaddr*)&local, sizeof(local));
@@ -27,17 +26,19 @@ public:
 
             sockaddr_in peer{};
             peer.sin_family = AF_INET;
-            peer.sin_port = htons(peer_port);
+            peer.sin_port = htons(port);
             inet_pton(AF_INET, peer_ip, &peer.sin_addr);
 
             connect(fd, (sockaddr*)&peer, sizeof(peer));
         } else {
             fd = accept(listen_fd, nullptr, nullptr);
         }
+
+        close(listen_fd);
     }
 
-    void send(const char* msg) {
-        ::send(fd, msg, strlen(msg), 0);
+    void send(const std::string& msg) {
+        ::send(fd, msg.data(), msg.size(), 0);
     }
 
     int receive(char* buf, int size) {
@@ -46,22 +47,27 @@ public:
 
     ~Peer() {
         close(fd);
-        close(listen_fd);
     }
 };
 
 int main(int argc, char** argv) {
     Peer peer(9000, argc > 1 ? argv[1] : nullptr);
 
-    char buf[1024];
+    std::thread receiver([&] {
+        char buf[1024];
 
-    while (true) {
-        int n = peer.receive(buf, sizeof(buf));
+        while (true) {
+            int n = peer.receive(buf, sizeof(buf));
+            if (n <= 0) break;
 
-        if (n <= 0)
-            break;
+            std::cout.write(buf, n);
+            std::cout.flush();
+        }
+    });
 
-        std::cout.write(buf, n);
-        std::cout.flush();
-    }
+    std::string msg;
+    while (std::getline(std::cin, msg))
+        peer.send(msg);
+
+    receiver.join();
 }
