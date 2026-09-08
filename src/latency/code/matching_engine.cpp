0b18bcda9254ebb155d28b917f9a4f44fd10d32d
odenpetersen <oden.petersen@gmail.com>
2026-09-08 22:18:48 +1000

    formatting

--- src/latency/code/matching_engine.cpp
+++ src/latency/code/matching_engine.cpp
@@ -5,23 +5,24 @@
 #include <string>
 
 int main(int argc, char** argv) {
-    Connection conn(9000, argc > 1 ? argv[1] : nullptr);
+	Connection conn(9000, argc > 1 ? argv[1] : nullptr);
 
-    std::thread receiver([&] {
-        char buf[1024];
+	std::thread receiver([&] {
+		char buf[1024];
 
-        while (true) {
-            int n = conn.receive(buf, sizeof(buf));
-            if (n <= 0) break;
+		while (true) {
+			int n = conn.receive(buf, sizeof(buf));
+			if (n <= 0) break;
 
-            std::cout.write(buf, n);
-            std::cout.flush();
-        }
-    });
+			std::cout.write(buf, n);
+			std::cout << std::endl;
+			std::cout.flush();
+		}
+	});
 
-    std::string msg;
-    while (std::getline(std::cin, msg))
-        conn.send(msg);
+	std::string msg;
+	while (std::getline(std::cin, msg))
+		conn.send(msg);
 
-    receiver.join();
+	receiver.join();
 }
